fd0b90df846fbcb51a06f147c751ffa11ce323f5
aditya <adityapatilsde@gmail.com>
2026-03-09 18:38:24 +0530

    Coded UC1 to Print OOPS to Console

---
+++ oops/src/main/java/org/example/uc1.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+
+
+//@author ADITYA
+//@version 1.0
+
+public class uc1 {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print OOPS to the console
+        System.out.println("OOPS");
+
+    }
+}
