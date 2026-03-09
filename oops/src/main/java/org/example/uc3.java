818cc6ab1d56ab3c7ec00457ea3320d6fd067150
aditya <adityapatilsde@gmail.com>
2026-03-09 19:33:44 +0530

    Coded Uc3

---
+++ oops/src/main/java/org/example/uc3.java
@@ -0,0 +1,57 @@
+package org.example;
+
+//@author ADITYA
+//@version 1.0
+public class uc3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " *****  ",
+                " *****  ",
+                " ****** ",
+                " *****  "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     * "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*       "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "******  ",
+                " *****  "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "*       ",
+                "      * "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "*       ",
+                "*     * "
+        ));
+
+        System.out.println(String.join(" ",
+                " *****  ",
+                " *****  ",
+                "*       ",
+                " *****  "
+        ));
+    }
+}
